295029812a662cf602e7c732221de2c8b29e7cbd
ramtinKh <ramtinkhoshnevis1@gmail.com>
2021-07-06 16:35:32 +0430

    add scoreboard structure

--- src/main/java/view/StageController.java
+++ src/main/java/view/StageController.java
@@ -9,6 +9,7 @@ import javafx.stage.Stage;
 import model.enums.Menu;
 import model.userProp.LoginUser;
 import model.userProp.User;
+import view.controller.ScoreboardView;
 
 import java.util.Objects;
 
@@ -22,7 +23,11 @@ public class StageController extends Application {
     public void start(Stage stage) throws Exception {
         stage.setTitle("YuGiOh!");
         LoginUser.setUser(new User("ali", "ali", "ali"));
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(Menu.DECKS_VIEW.getAddress())));
+        new User("ramtin", "rami", "1");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(Menu.SCOREBOARD_MENU.getAddress()));
+        Parent root = loader.load();
+        ScoreboardView controller = loader.getController();
+        controller.setDetails();
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
