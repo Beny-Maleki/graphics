785155b2f1c3784ba72c9878faddaabebade7670
Beny-Maleki <benyaminmaleki81@gmail.com>
2021-07-08 16:12:46 +0430

    continue some more GameView

--- src/main/java/view/controller/GameView.java
+++ src/main/java/view/controller/GameView.java
@@ -1,4 +1,15 @@
 package view.controller;
 
+import model.gameprop.GameInProcess;
+import model.gameprop.Player;
+import model.gameprop.gamemodel.Game;
+import model.userProp.User;
+import model.userProp.UserInfoType;
+
 public class GameView {
+
+    public void initialize() {
+        GameInProcess.setGame(new Game(new Player(User.getUserByUserInfo("Yaroo", UserInfoType.USERNAME), 1), new Player(User.getUserByUserInfo("ali", UserInfoType.USERNAME), 1), 3 ));
+
+    }
 }
