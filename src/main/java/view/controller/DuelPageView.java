a273e3d0e6826ef9e3af051b91190143cd427dcf
Arash <arashyadegari0402@gmail.com>
2021-07-08 18:49:27 -0700

    making duel page pop ups and properties and fix bug in register

--- src/main/java/view/controller/DuelPageView.java
+++ src/main/java/view/controller/DuelPageView.java
@@ -1,8 +1,11 @@
 package view.controller;
 
 import controller.menues.menuhandlers.menucontrollers.DuelMenuController;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import model.enums.Menu;
 
 import java.io.IOException;
@@ -12,17 +15,23 @@ public class DuelPageView {
     public Button twoPlayerGame;
     public Button onePlayerGame;
     public Button back;
+    public ChoiceBox roundNumber;
+    public Pane gameSetUpPopUp;
     private DuelMenuController controller;
 
     {
         controller = new DuelMenuController();
     }
 
+    @FXML
+    public void initialize() {
+    }
+
     public void run(MouseEvent event) throws IOException {
         if (event.getSource() == back) {
             controller.moveToPage(back, Menu.MAIN_MENU);
         } else if (event.getSource() == twoPlayerGame) {
-            controller.moveToPage(twoPlayerGame , Menu.ROCK_PAPER_SCISSOR_PAGE);
+            controller.moveToPage(twoPlayerGame, Menu.ROCK_PAPER_SCISSOR_PAGE);
         }
     }
 }
