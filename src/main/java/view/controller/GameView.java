27700a70a2db8aeff61b247ba3930da5d6bfd025
Beny-Maleki <benyaminmaleki81@gmail.com>
2021-07-09 18:24:38 +0430

    add GraveYard

--- src/main/java/view/controller/GameView.java
+++ src/main/java/view/controller/GameView.java
@@ -2,13 +2,26 @@ package view.controller;
 
 import animatefx.animation.FlipInX;
 import animatefx.animation.SlideInUp;
+
+import animatefx.animation.ZoomIn;
+import animatefx.animation.ZoomOut;
 import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+
+import model.cards.CardHouse;
+import model.cards.cardsProp.Card;
+import model.enums.Origin;
 import model.gameprop.BoardProp.GameHouse;
 import model.gameprop.BoardProp.HandHouse;
 import model.gameprop.BoardProp.MagicHouse;
@@ -22,6 +35,8 @@ import model.userProp.UserInfoType;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class GameView {
 
@@ -46,6 +61,10 @@ public class GameView {
     public Pane yourDeckPane;
     public Pane opponentDeckPane;
     public ImageView selectedCardImageView;
+    public AnchorPane root;
+
+    private Player playerYou;
+    private Player playerOpponent;
 
     public void initialize() throws FileNotFoundException {
         LoginUser.setUser(User.getUserByUserInfo("Yaroo", UserInfoType.USERNAME));
@@ -56,8 +75,8 @@ public class GameView {
         User opponent = User.getUserByUserInfo("ali", UserInfoType.USERNAME);
         //
 
-        Player playerYou = new Player(you, 0);
-        Player playerOpponent = new Player(opponent, 0);
+        playerYou = new Player(you, 0);
+        playerOpponent = new Player(opponent, 0);
 
         //TODO: replace this block with the result of Rock/paper/scissors
         Player firstPlayer = playerYou;
@@ -206,5 +225,130 @@ public class GameView {
 
         opponentUsername.setText("Username: " + opponent.getUsername());
         opponentNickname.setText("Nickname: " + opponent.getNickname());
+    }
+
+
+    private void showGraveYard(Player player) {
+        Pane graveYardPane = new Pane();
+
+
+
+        FlowPane graveYardFlowPane = new FlowPane();
+        graveYardFlowPane.setHgap(14);
+        graveYardFlowPane.setVgap(10);
+        graveYardFlowPane.setPrefWrapLength((player.getBoard().getGraveYard().getDestroyedCards().size()) * (14.222 + 109) + 28.444);
+        graveYardFlowPane.setPrefHeight(190);
+        graveYardFlowPane.setPadding(new Insets(14));
+        graveYardFlowPane.setStyle("-fx-background-image: url('/graphicprop/images/dirtyBoardBG.jpg'); -fx-background-size: cover");
+
+
+        ArrayList<Card> graveYardArrList = player.getBoard().getGraveYard().getDestroyedCards();
+        for (Card card : graveYardArrList) {
+            String nameWithoutSpaces = card.getName();
+            nameWithoutSpaces.replaceAll("\\s+", "");
+
+
+
+            FileInputStream fileInputStream;
+            Image image;
+            CardHouse cardHouse;
+            try {
+                ImageView imageView;
+                fileInputStream = new FileInputStream("/src/main/resources/graphicprop/images/Cards/Monsters/" + nameWithoutSpaces + ".jpg");
+                image = new Image(fileInputStream);
+                imageView = new ImageView(image);
+
+
+                imageView.setOnMouseEntered(e -> {
+                    imageView.setScaleX(1.1);
+                    imageView.setScaleY(1.1);
+
+                    DropShadow dropShadow = new DropShadow();
+                    imageView.setEffect(dropShadow);
+                });
+
+                imageView.setOnMouseExited(e -> {
+                    imageView.setScaleX(1);
+                    imageView.setScaleY(1);
+
+                    imageView.setEffect(null);
+
+                });
+
+                imageView.setOnMouseClicked(e -> {
+                    selectedCardImageView = imageView;
+                });
+
+                cardHouse = new CardHouse(card, imageView, image, Origin.GAME);
+                graveYardFlowPane.getChildren().add(imageView);
+            } catch (FileNotFoundException e) {
+                System.out.println("Ey baba :\"( aksa koshan pass!");
+            }
+        }
+
+        Button closeButton = new Button();
+        closeButton.setText("X");
+        closeButton.setOpacity(0.6);
+        closeButton.setStyle("-fx-background-color: red; -fx-border-color: black; -fx-border-radius: 5; -fx-background-radius: 5; -fx-cursor: hand");
+        closeButton.setPrefHeight(10);
+        closeButton.setPrefWidth(15);
+        closeButton.setOnMouseEntered(e -> {
+            closeButton.setOpacity(1);
+        });
+        closeButton.setOnMouseExited(e -> {
+            closeButton.setOpacity(0.6);
+        });
+        closeButton.setOnMouseClicked(e -> {
+            new ZoomOut(graveYardPane).play();
+            root.getChildren().remove(graveYardPane);
+            for (Node child : root.getChildren()) {
+                child.setDisable(false);
+                child.setOpacity(1);
+            }
+        });
+
+
+        Label title = new Label();
+        title.setText(player.getUser().getNickname() + "'s graveyard:");
+        title.setLayoutX(30);
+        title.setLayoutY(30);
+        title.setStyle("-fx-font-weight: bold; -fx-font-size: 20; -fx-text-fill: white");
+
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setPrefWidth(700);
+        scrollPane.setPrefHeight(190);
+        scrollPane.setLayoutX(30);
+        scrollPane.setLayoutY(60);
+        scrollPane.setPrefViewportHeight(190);
+        scrollPane.hbarPolicyProperty().setValue(ScrollPane.ScrollBarPolicy.ALWAYS);
+        scrollPane.vbarPolicyProperty().setValue(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.toFront();
+
+        graveYardFlowPane.setMinWidth(700);
+        scrollPane.setContent(graveYardFlowPane);
+
+        graveYardPane.toFront();
+        graveYardPane.setPrefHeight(300);
+        graveYardPane.setPrefWidth(760);
+        graveYardPane.setStyle("-fx-background-image: url('/graphicprop/images/brickwall.jpg'); -fx-background-size: cover");
+        graveYardPane.setLayoutX(150);
+        graveYardPane.setLayoutY(200);
+
+        graveYardPane.getChildren().addAll(closeButton, title, scrollPane);
+
+        for (Node child : root.getChildren()) {
+            child.setDisable(true);
+            child.setOpacity(0.3);
+        }
+        root.getChildren().add(graveYardPane);
+        new ZoomIn(graveYardPane).play();
+    }
+
+    public void run(MouseEvent mouseEvent) {
+        if (mouseEvent.getSource() == yourGraveyardPane) {
+            showGraveYard(playerYou);
+        } else if (mouseEvent.getSource() == opponentGraveyardPane) {
+            showGraveYard(playerOpponent);
+        }
     }
 }
